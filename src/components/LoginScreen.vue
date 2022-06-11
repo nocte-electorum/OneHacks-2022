3b4536d2767bdb4e2f353e7bb1067079d8766f76
Ad0pt3d <rafimiah2005@gmail.com>
2022-06-11 15:13:11 -0400

    log in button anim

--- src/components/LoginScreen.vue
+++ src/components/LoginScreen.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="login">  <!-- change this -->
+	<div :class="{ shake: disabled }" class="login">  <!-- change this -->
 		<h1>JitGuardian</h1>
 		<img src="../assets/JitGuardian.png" width="64">
 		<h4>User Name:</h4>
@@ -17,6 +17,11 @@
 import { invoke } from "@tauri-apps/api";
 
 export default {
+	data() {
+		return {
+			disabled: false
+		}
+	},
 	name: "LoginScreen",  // Change this
 	beforeCreate() {
 		document.querySelector('body').setAttribute('style', 'background:#3D3D3D')
@@ -35,7 +40,14 @@ export default {
 			).catch(() => {
 				let t = document.getElementById("errortext");
 				t.setAttribute('style', 'display: block;')
+				this.errorDisabled()
 			})
+		},
+		errorDisabled() {
+			this.disabled = true
+			setTimeout(() => {
+				this.disabled = false
+			}, 1500)
 		}
 	}
 }
@@ -112,4 +124,27 @@ p {
 	display: none;
 }
 /* padding format: top right bottom left */
+
+.shake {
+  animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+  transform: translate3d(0, 0, 0);
+}
+
+@keyframes shake {
+  10%, 90% {
+    transform: translate3d(-1px, 0, 0);
+  }
+
+  20%, 80% {
+    transform: translate3d(2px, 0, 0);
+  }
+
+  30%, 50%, 70% {
+    transform: translate3d(-4px, 0, 0);
+  }
+
+  40%, 60% {
+    transform: translate3d(4px, 0, 0);
+  }
+}
 </style>
