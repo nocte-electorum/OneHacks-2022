a4bd98f705e63fb2758f28f1f8f5f0c980109723
Raforawesome <ubuntuhasnoroblox@gmail.com>
2022-06-11 11:02:50 -0400

    deleted dummy funcion

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,11 +9,6 @@ fn print_hello() {  // dummy function to test client -> server boundary
 	println!("Hello world!");
 }
 
-#[tauri::comand]
-fn print_hello_2() {
-	println!("Hello world 2");
-}
-
 
 fn main() {
   tauri::Builder::default()
