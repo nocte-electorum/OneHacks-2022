9218ac88a887c967af7471b36b3be18ff28f1eaf
shawarma-s <pepperghost51@gmail.com>
2022-06-11 16:49:12 -0400

    removed unused code from commands.rs

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,9 +1,4 @@
 #[tauri::command]
-pub fn print_hello() {  // dummy function to test client -> server boundary
-	println!("Hello world!");
-}
-
-#[tauri::command]
 pub async fn login(username: String, password: String) -> Result<(), String> {
 	Err("Wrong username or password.".to_string())
 	// println!("Attemped login with {username} and {password}.");
