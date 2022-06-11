0ef620828904428adcc46c0d47053c0d344f004d
Raforawesome <ubuntuhasnoroblox@gmail.com>
2022-06-11 12:20:39 -0400

    Made a template vue for easier cloning

---
+++ src/components/TemplateFile.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="template">  <!-- change this -->
+		<h1>Template file</h1>
+		<p>This is a template file.</p>
+	</div>
+</template>
+
+
+<script>
+export default {
+	name: "TemplateFile"  // Change this
+}
+</script>
