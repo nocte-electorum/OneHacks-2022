a50f770fb2fb5753f895b10b84c024245b541af3
Raforawesome <ubuntuhasnoroblox@gmail.com>
2022-06-12 20:10:02 -0400

    Rewrote get_key() function to be static

--- src-tauri/src/fs.rs
+++ src-tauri/src/fs.rs
@@ -52,6 +52,24 @@ pub fn get_key(len: usize) -> (String, String) {
 		let (key, nonce) = (lines[0].trim().into(), lines[1].trim().into());
 		return(key, nonce)
 	} else {
+		(crate::randstring::rand_string(32), crate::randstring::rand_nonce())
+	}
+}
+
+pub fn get_key_dyn(len: usize) -> (String, String) {
+	data_init();
+	let mut cwd = std::env::current_dir().unwrap_or_else(|_| {
+		println!("FATAL: Failed to get current working directory.");
+		std::process::exit(1);
+	});
+	cwd = cwd.join("data");
+
+	let file = std::fs::read_to_string(cwd.join("1"));
+	if let Ok(s) = file && s.len() > len {
+		let lines = s.lines().collect::<Vec<&str>>();
+		let (key, nonce) = (lines[0].trim().into(), lines[1].trim().into());
+		return(key, nonce)
+	} else {
 		let newkey: String = crate::randstring::rand_string(len);
 		let newnonce: String = crate::randstring::rand_nonce();
 		let to_write: String = format!("{}\n{}", newkey, newnonce);
